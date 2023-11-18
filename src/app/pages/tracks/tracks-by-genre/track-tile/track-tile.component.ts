46e353992f0c924c3df3a80f2c57854ba4ff855b
Oskarovsky <oskar.slyk@gmail.com>
2023-11-18 17:50:09 +0100

    Update angular views

--- src/app/pages/tracks/tracks-by-genre/track-tile/track-tile.component.ts
+++ src/app/pages/tracks/tracks-by-genre/track-tile/track-tile.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewChecked, Component, Input, SecurityContext} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, Input, Renderer2, SecurityContext, ViewChild} from '@angular/core';
 import {Track} from '../../track/model/track';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {UploadFileService} from '../../../../services/storage/upload-file.service';
@@ -17,6 +17,8 @@ export class TrackTileComponent implements AfterViewChecked {
   @Input() currentPage: number;
   @Input() isLoggedIn: boolean;
 
+  @ViewChild('trackAreaDiv', { static: false }) trackAreaDiv: ElementRef;
+
   coversToShow: Map<number, any> = new Map<number, any>();
   mapa: Map<number, SafeResourceUrl> = new Map<number, SafeResourceUrl>();
   divShowMapa: Map<number, boolean> = new Map<number, boolean>();
@@ -24,11 +26,13 @@ export class TrackTileComponent implements AfterViewChecked {
   idList: Array<number> = new Array<number>();
 
   constructor(public sanitizer: DomSanitizer,
+              private renderer: Renderer2,
               public navigationComponent: NavigationComponent,
               public fileService: UploadFileService) { }
 
   ngAfterViewChecked() {
     this.tracks.forEach(t => {
+      this.track.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.track.urlPlugin.replace("1000", "800"))
       if (!this.idList.includes(t.id)) {
         this.idList.push(t.id);
         this.getCoverImage(t.id);
@@ -48,8 +52,10 @@ export class TrackTileComponent implements AfterViewChecked {
       let urlMobile: string = this.sanitizer.sanitize(SecurityContext.RESOURCE_URL, this.track.safeUrl);
       this.mapa.set(this.track.id, urlMobile);
     } else {
-      let informacja = this.track.safeUrl.valueOf()
-      console.log('OSKAR: ' + informacja)
+      const trackAreaWidth = this.trackAreaDiv.nativeElement.offsetWidth;
+      this.track.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        this.track.urlPlugin.replace("1000", trackAreaWidth)
+      )
       this.mapa.set(this.track.id, this.track.safeUrl);
     }
   }
