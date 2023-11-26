4a1031e3c7eead7a969a0d30bafd3419c6c1c58c
Oskarovsky <oskar.slyk@gmail.com>
2023-11-26 13:55:27 +0100

    style refactoring;

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,7 +11,7 @@ import {NavigationComponent} from "../../skeleton/navigation/navigation.componen
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
